3ba7306ce9369c260cfa6bb8b93f9118db77596b
steffen2 <steffenb198@aol.com>
2012-03-06 20:24:24 +0100

    Added basic money support.

--- src/main/java/com/iBaby/commands/SummonCommand.java
+++ src/main/java/com/iBaby/commands/SummonCommand.java
@@ -2,6 +2,7 @@ package com.iBaby.commands;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 
 import com.iBaby.Command;
 import com.iBaby.Configuration;
@@ -20,11 +21,31 @@ public class SummonCommand extends Command {
 			if(iBaby.getIronBabys(p.getName()).size() < Configuration.max) {
 				if(param("amount") != null) {
 					int c = 0;
+					if(Configuration.enablePrice) {
+						double amount = Configuration.price * Integer.parseInt(param("amount"));
+						if(iBaby.economy.has(p.getName(), amount)) {
+							iBaby.economy.withdrawPlayer(p.getName(), amount);
+							sender.sendMessage(ChatColor.GREEN + " You paid " + amount);
+						}else{
+							sender.sendMessage(ChatColor.RED + " You don't have " + amount + " money");
+							return;
+						}
+					}
 					for(c = 0; c < Integer.parseInt(param("amount")); c++) {
 						iBaby.spawnIronBaby(p.getLocation(), p);
 					}
 					sender.sendMessage("Now you have " + c + " iron babysitters!");
 				}else{
+					if(Configuration.enablePrice) {
+						double amount = Configuration.price;
+						if(iBaby.economy.has(p.getName(), amount)) {
+							iBaby.economy.withdrawPlayer(p.getName(), amount);
+							sender.sendMessage(ChatColor.GREEN + " You paid " + amount);
+						}else{
+							sender.sendMessage(ChatColor.RED + " You don't have " + amount + " money");
+							return;
+						}
+					}
 					iBaby.spawnIronBaby(p.getLocation(), p);
 					sender.sendMessage("Now you have an iron babysitter!");
 				}
