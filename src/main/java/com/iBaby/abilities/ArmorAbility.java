4948b90fa48235198fe78380945e5962721b1f34
steffen <steffenb198@aol.com>
2012-03-14 22:24:23 +0100

    Implemented Damage Ability Handling.

--- src/main/java/com/iBaby/abilities/ArmorAbility.java
+++ src/main/java/com/iBaby/abilities/ArmorAbility.java
@@ -26,69 +26,70 @@ public class ArmorAbility extends Ability {
 	 */
 	public static Ability matches(ItemStack itemstack) {
 		Ability handle = null;
+		CraftItemStack stack = new CraftItemStack(itemstack);
 		/* LEATHER */
-		if(new CraftItemStack(itemstack).getType() == Material.LEATHER_CHESTPLATE) {
+		if(stack.getType() == Material.LEATHER_CHESTPLATE) {
 				handle = new ArmorAbility(9);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.LEATHER_BOOTS) {
+		else if(stack.getType() == Material.LEATHER_BOOTS) {
 				handle = new ArmorAbility(7);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.LEATHER_HELMET) {
+		else if(stack.getType() == Material.LEATHER_HELMET) {
 				handle = new ArmorAbility(6);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.LEATHER_LEGGINGS) {
+		else if(stack.getType() == Material.LEATHER_LEGGINGS) {
 				handle = new ArmorAbility(8);
 		}
 		/* GOLD */
-		else if(new CraftItemStack(itemstack).getType() == Material.GOLD_CHESTPLATE) {
+		else if(stack.getType() == Material.GOLD_CHESTPLATE) {
 				handle = new ArmorAbility(12);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.GOLD_BOOTS) {
+		else if(stack.getType() == Material.GOLD_BOOTS) {
 				handle = new ArmorAbility(10);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.GOLD_HELMET) {
+		else if(stack.getType() == Material.GOLD_HELMET) {
 				handle = new ArmorAbility(8);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.GOLD_LEGGINGS) {
+		else if(stack.getType() == Material.GOLD_LEGGINGS) {
 				handle = new ArmorAbility(11);
 		}
 		/* CHAIN */
-		else if(new CraftItemStack(itemstack).getType() == Material.CHAINMAIL_CHESTPLATE) {
+		else if(stack.getType() == Material.CHAINMAIL_CHESTPLATE) {
 				handle = new ArmorAbility(26);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.CHAINMAIL_BOOTS) {
+		else if(stack.getType() == Material.CHAINMAIL_BOOTS) {
 				handle = new ArmorAbility(21);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.CHAINMAIL_HELMET) {
+		else if(stack.getType() == Material.CHAINMAIL_HELMET) {
 				handle = new ArmorAbility(18);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.CHAINMAIL_LEGGINGS) {
+		else if(stack.getType() == Material.CHAINMAIL_LEGGINGS) {
 				handle = new ArmorAbility(25);
 		}
 		/* Iron @NERF */
-		else if(new CraftItemStack(itemstack).getType() == Material.IRON_CHESTPLATE) {
+		else if(stack.getType() == Material.IRON_CHESTPLATE) {
 				handle = new ArmorAbility(17);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.IRON_BOOTS) {
+		else if(stack.getType() == Material.IRON_BOOTS) {
 				handle = new ArmorAbility(14);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.IRON_HELMET) {
+		else if(stack.getType() == Material.IRON_HELMET) {
 				handle = new ArmorAbility(11);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.IRON_LEGGINGS) {
+		else if(stack.getType() == Material.IRON_LEGGINGS) {
 				handle = new ArmorAbility(15);
 		}
 		/* Diamond @NERF2 */
-		else if(new CraftItemStack(itemstack).getType() == Material.DIAMOND_CHESTPLATE) {
+		else if(stack.getType() == Material.DIAMOND_CHESTPLATE) {
 				handle = new ArmorAbility(26);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.DIAMOND_BOOTS) {
+		else if(stack.getType() == Material.DIAMOND_BOOTS) {
 				handle = new ArmorAbility(21);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.DIAMOND_HELMET) {
+		else if(stack.getType() == Material.DIAMOND_HELMET) {
 				handle = new ArmorAbility(18);
 		}
-		else if(new CraftItemStack(itemstack).getType() == Material.DIAMOND_LEGGINGS) {
+		else if(stack.getType() == Material.DIAMOND_LEGGINGS) {
 				handle = new ArmorAbility(23);
 		}
 		return handle;
--- src/main/java/com/iBaby/abilities/DamageAbility.java
+++ src/main/java/com/iBaby/abilities/DamageAbility.java
@@ -1,5 +1,10 @@
 package com.iBaby.abilities;
 
+import net.minecraft.server.ItemStack;
+
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+
 /**
  * The additional damage a golem deals through weapons
  * @author steffengy
@@ -15,5 +20,75 @@ public class DamageAbility extends Ability {
 	@Override
 	public int getAdditionalDamage() {
 		return this.damage;
+	}
+	
+	/**
+	 * Matches the itemstack
+	 * @param itemstack
+	 * @return Either the ability or null
+	 */
+	public static Ability matches(ItemStack itemstack) {
+		Ability handle = null;
+		CraftItemStack stack = new CraftItemStack(itemstack);
+		/* WOOD */
+		if(stack.getType() == Material.WOOD_SWORD) {
+			handle = new DamageAbility(2);
+		}
+		else if(stack.getType() == Material.WOOD_AXE) {
+			handle = new DamageAbility(1);
+		}
+		else if(stack.getType() == Material.WOOD_PICKAXE) {
+			handle = new DamageAbility(1);
+		}
+		/* GOLD */
+		else if(stack.getType() == Material.GOLD_SWORD) {
+			handle = new DamageAbility(2);
+		}
+		else if(stack.getType() == Material.GOLD_AXE) {
+			handle = new DamageAbility(1);
+		}
+		else if(stack.getType() == Material.GOLD_PICKAXE) {
+			handle = new DamageAbility(1);
+		}
+		/* STONE */
+		else if(stack.getType() == Material.STONE_SWORD) {
+			handle = new DamageAbility(2);
+		}
+		else if(stack.getType() == Material.STONE_AXE) {
+			handle = new DamageAbility(1);
+		}
+		else if(stack.getType() == Material.STONE_PICKAXE) {
+			handle = new DamageAbility(1);
+		}
+		else if(stack.getType() == Material.STONE_SPADE) {
+			handle = new DamageAbility(1);
+		}
+		/* IRON */
+		else if(stack.getType() == Material.IRON_SWORD) {
+			handle = new DamageAbility(3);
+		}
+		else if(stack.getType() == Material.IRON_AXE) {
+			handle = new DamageAbility(2);
+		}
+		else if(stack.getType() == Material.IRON_PICKAXE) {
+			handle = new DamageAbility(2);
+		}
+		else if(stack.getType() == Material.IRON_SPADE) {
+			handle = new DamageAbility(1);
+		}
+		/* DIAMOND */
+		else if(stack.getType() == Material.DIAMOND_SWORD) {
+			handle = new DamageAbility(4);
+		}
+		else if(stack.getType() == Material.DIAMOND_AXE) {
+			handle = new DamageAbility(3);
+		}
+		else if(stack.getType() == Material.DIAMOND_PICKAXE) {
+			handle = new DamageAbility(2);
+		}
+		else if(stack.getType() == Material.DIAMOND_SPADE) {
+			handle = new DamageAbility(2);
+		}
+		return handle;
 	}
 }
